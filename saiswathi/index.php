41e5c684e1607c600022c831063e98cbacc18032
saiswathi9 <35580410+saiswathi9@users.noreply.github.com>
2021-02-23 20:38:51 -0600

    pushing my website

---
+++ saiswathi/index.php
@@ -0,0 +1,492 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+
+<html class="no-js"> <!--<![endif]-->
+
+    <head>
+        <meta charset="utf-8">
+        <title>Web Designer Template</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="css/flexslider.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/line-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/elegant-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/lightbox.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/theme-1.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/custom.css" rel="stylesheet" type="text/css" media="all"/>
+        <!--[if gte IE 9]>
+        	<link rel="stylesheet" type="text/css" href="css/ie9.css" />
+		<![endif]-->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,600,700%7CRaleway:700' rel='stylesheet' type='text/css'/>
+        <script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"/>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+    <script src="js/chatbot.js"></script>
+    <link rel="stylesheet" href="css/chatbot.css" type="text/css"/>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ 
+        ga('create', 'UA-76380861-2', 'auto');
+        ga('send', 'pageview');
+ 
+    </script>
+    </head>
+    <body>
+    	<div class="loader">
+    		<div class="spinner">
+			  <div class="double-bounce1"></div>
+			  <div class="double-bounce2"></div>
+			</div>
+    	</div>
+				
+		<div class="nav-container">
+			<nav class="simple-bar top-bar">
+				<div class="container">
+				
+				
+					<div class="row nav-menu">
+						<div class="col-md-3 col-sm-3 columns">
+							<a href="index.php">
+							<img class="logo logo-dark" alt="Logo" src="img/Capture.png">
+						</div>
+					
+						<div class="col-md-9 col-sm-9 columns text-right">
+							<ul class="menu">
+								<a></a><li><a href="index.html" target="_self">Home</a></li><li><a href="contact.html" target="_self">Contact</a></li>
+								<a class="weatherwidget-io" href="https://forecast7.com/en/38d95n92d33/columbia/" data-label_1="COLUMBIA" data-label_2="WEATHER" data-theme="original" >COLUMBIA WEATHER</a>
+								<a href="https://www.weatherforyou.com/weather/missouri/columbia.html">
+<img src="https://www.weatherforyou.net/fcgi-bin/hw3/hw3.cgi?config=png&forecast=hourly&alt=hwihourly&place=columbia&state=mo&country=us&hwvbg=&hwvtc=&hwvdisplay=" width="175" height="90" border="0" alt="Latest Columbia, Missouri, weather"></a>
+							</ul>
+		
+							
+						</div>
+					</div>
+					
+					<div class="mobile-toggle">
+						<i class="icon icon_menu"></i>
+					</div>
+					
+				</div>
+			</nav>
+		
+		</div>
+		
+		<div class="main-container">
+			<header class="header-icons overlay">
+					<div class="background-image-holder parallax-background">
+						<img class="background-image" alt="Background Image" src="img/download.jpg">
+					</div>
+					
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-12">
+								<h1 class="text-white">
+									Hello, I'm Sai Swathi Sivarathri.<br>I am a Developer.</h1>
+							</div>
+						</div>
+						
+						<div class="row">
+							<div class="col-sm-4">
+								<i class="icon icon-desktop text-white"></i>
+								<h2 class="text-white">Software <br>Development</h2>
+							</div>
+							
+							<div class="col-sm-4">
+								<i class="icon icon-desktop text-white"></i>
+								<h2 class="text-white">Deep Learning</h2>
+							</div>
+							
+							<div class="col-sm-4">
+								<i class="icon text-white icon-strategy"></i>
+								<h2 class="text-white">Artist</h2>
+							</div>
+						</div>
+		
+					</div>	
+			</header>
+			
+			<section class="skill-bars">
+		
+				<div class="container">
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 text-center">
+							<h1>My skills</h1>
+							<p class="lead">
+								As a creative problem solver, I am constantly enamored in figuring out how the world around me works. From a young age, I was always taking things apart to see how they operated and now as a computer scientist, I love being able to make my own creations function. I love the tools coding gives me to be able to create something out of nothing and I am looking to enrich my knowledge through work experience. 
+							</p>	
+						</div>
+					</div>
+					
+					<div class="row">
+						<div class="col-md-6 col-sm-12">
+							<div class="skills-left skills">
+								<h3>ML/NLP</h3>
+								<ul class="skills-ul">
+									<li>
+										<span>GANs</span>
+										<div class="skill-bar-holder">
+											<div class="skill-capacity col-xs-11"></div>
+										</div>
+									</li>
+									
+									<li>
+										<span>SVM</span>
+										<div class="skill-bar-holder">
+											<div class="skill-capacity col-xs-9"></div>
+										</div>
+									</li>
+									
+									<li>
+										<span>Sentiment Analysis</span>
+										<div class="skill-bar-holder">
+											<div class="skill-capacity col-xs-7"></div>
+										</div>
+									</li>
+									
+									<li>
+										<span>Pattern Recognition</span>
+										<div class="skill-bar-holder">
+											<div class="skill-capacity col-xs-6"></div>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+						
+						<div class="col-md-6 col-sm-12">
+							<div class="skills-right skills">
+								<h3>Developing</h3>
+								<ul class="skills-ul">
+									<li>
+										<span>Java</span>
+										<div class="skill-bar-holder">
+											<div class="skill-capacity col-xs-11"></div>
+										</div>
+									</li>
+									
+									<li>
+										<span>Javascript / Jquery</span>
+										<div class="skill-bar-holder">
+											<div class="skill-capacity col-xs-9"></div>
+										</div>
+									</li>
+									
+									<li>
+										<span>PHP &amp; MySQL</span>
+										<div class="skill-bar-holder">
+											<div class="skill-capacity col-xs-7"></div>
+										</div>
+									</li>
+									
+									<li>
+										<span>HTML &amp; CSS3</span>
+										<div class="skill-bar-holder">
+											<div class="skill-capacity col-xs-6"></div>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					
+		
+				</div>
+			</section>
+			
+		<!--	<section class="no-pad-bottom projects-gallery">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 text-center">
+							<h1>Check out my latest projects</h1>
+							<p class="lead">
+								Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. 
+							</p>
+						</div>
+					</div>
+					
+				</div>
+				
+				<div class="projects-wrapper clearfix">
+					
+					
+					<div class="projects-container">
+					
+						<div class="col-md-6 col-sm-12 no-pad project development image-holder">
+							<div class="background-image-holder">
+								<img class="background-image" alt="Background Image" src="img/project_1.jpg">
+							</div>
+							<div class="hover-state">
+								<div class="align-vertical">
+									<h1 class="text-white"><strong>Project 1</strong>&nbsp;Lorem ipsum</h1>
+									<p class="text-white">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</p>
+									<a href="#" class="btn btn-primary btn-white">See Project</a>
+								</div>
+							</div>
+						</div>
+						
+						<div class="col-md-6 col-sm-12 no-pad project development image-holder">
+							<div class="background-image-holder">
+								<img class="background-image" alt="Background Image" src="img/project_2.jpg">
+							</div>
+							<div class="hover-state">
+								<div class="align-vertical">
+									<h1 class="text-white"><strong>Project 2</strong>&nbsp;Dolor sit</h1>
+									<p class="text-white">Quisquam est, qui dolorem ipsum quia dolor</p>
+									<a href="#" class="btn btn-primary btn-white">See Project</a>
+								</div>
+							</div>
+						</div>
+						
+						<div class="col-md-4 col-sm-6 no-pad project print image-holder">
+							<div class="background-image-holder">
+								<img class="background-image" alt="Background Image" src="img/project_3.jpg">
+							</div>
+							<div class="hover-state">
+								<div class="align-vertical">
+									<h1 class="text-white"><span>Project 3&nbsp;</span>Sit amet</h1>
+									<p class="text-white">Incidunt labore dolore magnam aliquam quaerat voluptatem</p>
+									<a href="#" class="btn btn-primary btn-white">See Project</a>
+								</div>
+							</div>
+						</div>
+						
+						<div class="col-md-4 col-sm-6 no-pad project branding image-holder">
+							<div class="background-image-holder">
+								<img class="background-image" alt="Background Image" src="img/project_4.jpg">
+							</div>
+							<div class="hover-state">
+								<div class="align-vertical">
+									<h1 class="text-white"><strong>Project 4</strong>&nbsp;Lorem dolor</h1>
+									<p class="text-white">Labore et dolore magnam aliquam quaerat</p>
+									<a href="#" class="btn btn-primary btn-white">See Project</a>
+								</div>
+							</div>
+						</div>
+						
+						<div class="col-md-4 col-sm-6 no-pad project print image-holder">
+							<div class="background-image-holder">
+								<img class="background-image" alt="Background Image" src="img/project_5.jpg">
+							</div>
+							<div class="hover-state">
+								<div class="align-vertical">
+									<h1 class="text-white"><span>Project 5&nbsp;</span>Dolor sit amet</h1>
+									<p class="text-white">Dolore magnam aliquam quaerat</p>
+									<a href="#" class="btn btn-primary btn-white">See Project</a>
+								</div>
+							</div>
+						</div>
+					
+					</div>
+					
+				</div>
+				
+			</section>-->
+			
+			<section class="timeline-1">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 text-center">
+							<h1>Here is a little of my story</h1>
+							<p class="lead">
+								 
+							</p>	
+						</div>
+					</div>
+					
+					<div class="row">
+						<div class="col-sm-12">
+						<div class="timeline-event">
+								<div class="col-sm-6 col-md-push-6">
+									<h2>2019</h2>
+									<h5>Student Coordinator for REU, University of Missouri-Columbia<br></h5>
+								</div>
+								
+								<div class="middle">
+									<i class="icon icon-video"></i>
+									<div class="vertical-line"></div>
+								</div>
+								
+								<div class="col-sm-6 col-md-pull-6">
+									<p>I had a chance to supervise and interact with REU students with weekly meetings, making sure they do a daily signup and provide some research process guidance as they get started at Mizzou initially</p>
+								</div>
+								
+								
+							</div>
+						<div class="timeline-event">
+								<div class="col-sm-6">
+									<h2>2018-2019</h2>
+									<h5>Research Assistant at Networking and Multimedia Lab, University of Missouri-Columbia<br></h5>
+								</div>
+								
+								<div class="middle">
+									<i class="icon icon-map"></i>
+									<div class="vertical-line"></div>
+								</div>
+								
+								<div class="col-sm-6">
+									<p>Worked on ScholarFinder: Knowledge Embedding based Expert Recommendations using Deep Generative Model as part 
+									of my Master's Thesis where I had a chance to design and develop a generative model to find Scholars in a 
+									research community by a novel knowledge embedding technique. I also developed a Conversational Agent using 
+Google's DialogFlow which is an embeddable Chatbot widget on the CyNeuro.org portal that has increased website's usage by 40%</p>
+								</div>
+								
+								
+							</div>
+							<div class="timeline-event">
+								<div class="col-sm-6 col-md-push-6">
+									<h2>2017-2019</h2>
+									<h5>Web Developer at Technology Services, University of Missouri-Columbia<br></h5>
+								</div>
+								
+								<div class="middle">
+									<i class="icon icon-trophy"></i>
+									<div class="vertical-line"></div>
+								</div>
+								
+								<div class="col-sm-6 col-md-pull-6">
+									<p>My work involved designing and developing University internal web applications using JavaScript, PHP and MySQL. Also
+Optimize SQL queries and develop stored procedures and functions for those applications.&nbsp;</p>
+								</div>
+								
+								
+							</div>
+							
+							<div class="timeline-event">
+								<div class="col-sm-6">
+									<h2>2018</h2>
+									<h5>Software Development Intern at AT&T</h5>	
+								</div>
+								
+								<div class="middle">
+									<i class="icon icon-target"></i>
+									<div class="vertical-line"></div>
+								</div>
+								
+								<div class="col-sm-6">
+									<p>Worked on three interesting projects where I'd chance to build Chatbot 2.0 along with User Interface to status check, for Chief Compliance Office
+I worked with a team of two interns to develop a Dynamic Survey Tool to create, modify and share questionnaire among members of the organization. The third interesting project was to 
+do Sentiment Analysis over customer reviews to find the reason for drop in DirecTV Subscription</p>
+									
+								</div>
+							</div>
+							
+							<div class="timeline-event">
+								<div class="col-sm-6 col-md-push-6">
+									<h2>2014-2016</h2>
+									<h5>Software Engineer at Tech Mahindra</h5>
+								</div>
+								
+								<div class="middle">
+									<i class="icon icon-trophy"></i>
+									<div class="vertical-line"></div>
+								</div>
+								
+								<div class="col-sm-6 col-md-pull-6">
+									<p>
+  Built a Risk Analyzer web app for Microsoft using C++ and developed Web Front End (GUI) using HTML/HTML5, CSS, jQuery, JSON, AJAX
+  using web services through REST framework. Developed Unit Tests for Forms, Views and Models using unit-test module in C++</p>
+								</div>
+								
+								
+							</div>
+							
+							<div class="timeline-event">
+								<div class="col-sm-6">
+									<h2>2013</h2>
+									<h5>Intern at State Bank of India</h5>	
+								</div>
+								
+								<div class="middle">
+									<i class="icon icon-target"></i>
+									
+									<div class="vertical-line"></div>
+								</div>
+								
+								<div class="col-sm-6">
+									<p>Developed web application, Merchant Acquiring Business – Problem Management Software, using PHP and MySQL for Branch employees to interact with Local Head Office employees to report the problems regarding the Point of Sale (PoS) machine and acquire solutions									
+								</div>
+							</div>
+							
+						</div>
+					</div>
+					
+		
+				</div>
+			</section>
+		</div>
+		
+		<div class="footer-container">
+		
+			
+		
+			
+		
+			<footer class="bg-primary short-2">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<span class="text-white">© 2019 Sai Swathi - All Rights Reserved. </a></span>
+							<br>
+							<br>
+							<i class="icon text-white icon_mail"></i>
+							<a href="contact.html" class="text-white"><span class="text-white"> Get in touch with me <i class="icon arrow_right"></i></span></a>
+						</div>
+					</div>
+				</div>
+				<!--<div class="topstrip" id="topstrip">Powered by <a href="https://www.miningbusinessdata.com" style="color:#d3d3d3"> MiningBusinessData</a>&nbsp;&nbsp;&nbsp;</div>
+<div class="topbar" id="chat-text">
+</div>
+<form>
+    <span style="width:100%;">
+        <input class="inputbox"
+               placeholder="Write something and press Enter..." id="message" name="date" value="">
+    </span>
+    <input name="submit" type="hidden" value="Submit">
+</form>
+<?php
+$sessionID = uniqid('',true);
+include('starter.php');
+?>
+<span style="display: none;" id="sessionId">
+        <?php
+        echo $sessionID;
+        ?>
+    </span> -->
+
+	<iframe
+    allow="microphone;"
+    width="350"
+    height="430"
+    src="https://console.dialogflow.com/api-client/demo/embedded/1381c36c-dabe-4fb6-923a-30e0231c9c6b">
+</iframe>
+			</footer>
+		</div>
+				
+		<script src="https://www.youtube.com/iframe_api"></script>
+		<script src="js/jquery.min.js"></script>
+        <script src="js/jquery.plugin.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.flexslider-min.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/skrollr.min.js"></script>
+        <script src="js/spectragram.min.js"></script>
+        <script src="js/scrollReveal.min.js"></script>
+        <script src="js/isotope.min.js"></script>
+        <script src="js/twitterFetcher_v10_min.js"></script>
+        <script src="js/lightbox.min.js"></script>
+        <script src="js/jquery.countdown.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+				
